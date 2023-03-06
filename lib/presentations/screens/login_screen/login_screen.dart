65caec3d8e1e291a8d84464c2daee28ac823fdd3
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-03-07 00:48:27 +0700

    add sign up textspan gesturedetector

--- lib/presentations/screens/login_screen/login_screen.dart
+++ lib/presentations/screens/login_screen/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:one_one_learn/configs/constants/colors.dart';
@@ -138,6 +139,7 @@ class LoginScreen extends StatelessWidget {
                     children: [
                       TextSpan(
                         text: ' ${S.current.signUp}',
+                        recognizer: TapGestureRecognizer()..onTap = () {},
                         style: TextStyle(
                           color: AppColors.primaryBlue400,
                           fontSize: Dimens.getProportionalScreenWidth(context, 17),
