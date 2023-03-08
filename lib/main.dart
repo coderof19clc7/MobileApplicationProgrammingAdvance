c8c3ec36b49a0651ca9c750dde426a9c842d787f
coderof19clc7 <mailluubaihoc@gmail.com>
2023-03-08 17:46:12 +0700

    update main for theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:one_one_learn/configs/app_configs/stylings/app_themes.dart';
 import 'package:one_one_learn/generated/l10n.dart';
 import 'package:one_one_learn/presentations/screens/course_information_screen/course_information_screen.dart';
 import 'package:one_one_learn/presentations/screens/courses_screen/courses_screen.dart';
+import 'package:one_one_learn/presentations/screens/forgot_password_screen/forgot_password_screen.dart';
+import 'package:one_one_learn/presentations/screens/lesson_history_screen/lesson_history_screen.dart';
+import 'package:one_one_learn/presentations/screens/lesson_history_screen/widgets/lesson_history_card.dart';
+import 'package:one_one_learn/presentations/screens/login_screen/login_screen.dart';
+import 'package:one_one_learn/presentations/screens/on_boarding_screen/on_boarding_screen.dart';
+import 'package:one_one_learn/presentations/screens/sign_up_screen/sign_up_screen.dart';
 import 'package:one_one_learn/presentations/screens/tutor_information_screen/tutor_information_screen.dart';
 import 'package:one_one_learn/presentations/screens/tutors_screen/tutors_screen.dart';
 import 'package:one_one_learn/presentations/screens/upcoming_classes_screen/upcoming_classes_screen.dart';
@@ -18,9 +25,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: AppThemes.lightTheme,
+      darkTheme: AppThemes.darkTheme,
       localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -28,7 +34,7 @@ class MyApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      home: const UpcomingClassesScreen(),
+      home: const TutorInformationScreen(),
     );
   }
 }
