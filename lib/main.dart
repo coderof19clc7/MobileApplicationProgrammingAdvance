69702ab1caa046d3410d79d8df5ed820d97ffe6a
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-03-10 00:53:13 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:one_one_learn/configs/app_configs/app_router.dart';
 import 'package:one_one_learn/configs/app_configs/stylings/app_themes.dart';
 import 'package:one_one_learn/generated/l10n.dart';
 import 'package:one_one_learn/presentations/screens/course_information_screen/course_information_screen.dart';
@@ -7,6 +8,7 @@ import 'package:one_one_learn/presentations/screens/courses_screen/courses_scree
 import 'package:one_one_learn/presentations/screens/forgot_password_screen/forgot_password_screen.dart';
 import 'package:one_one_learn/presentations/screens/lesson_history_screen/lesson_history_screen.dart';
 import 'package:one_one_learn/presentations/screens/lesson_history_screen/widgets/lesson_history_card.dart';
+import 'package:one_one_learn/presentations/screens/lesson_information_screen/lesson_information_screen.dart';
 import 'package:one_one_learn/presentations/screens/login_screen/login_screen.dart';
 import 'package:one_one_learn/presentations/screens/on_boarding_screen/on_boarding_screen.dart';
 import 'package:one_one_learn/presentations/screens/sign_up_screen/sign_up_screen.dart';
@@ -25,6 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      onGenerateRoute: AppRouter.onGenerateRoute,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
       localizationsDelegates: const [
@@ -34,7 +37,7 @@ class MyApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      home: const TutorInformationScreen(),
+      home: const CoursesScreen(),
     );
   }
 }
