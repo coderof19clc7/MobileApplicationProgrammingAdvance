bc4fbdad850c7b3c8dfb275b76c3a11b054b3129
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-03-15 20:56:19 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:one_one_learn/configs/app_configs/app_router.dart';
 import 'package:one_one_learn/configs/app_configs/stylings/app_themes.dart';
 import 'package:one_one_learn/generated/l10n.dart';
-import 'package:one_one_learn/presentations/screens/booking/booking_screen.dart';
-import 'package:one_one_learn/presentations/screens/main_screen/main_screen.dart';
 import 'package:one_one_learn/presentations/screens/on_boarding/on_boarding_page.dart';
 
 void main() {
