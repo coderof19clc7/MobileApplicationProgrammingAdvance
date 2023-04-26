ceaed26b62f8a257949c240b0a40ae62fb8f4ff2
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-04-26 15:56:18 +0700

    Update booking_repository.dart

--- lib/core/network/repositories/booking_repository.dart
+++ lib/core/network/repositories/booking_repository.dart
@@ -1,6 +1,6 @@
 import 'package:one_one_learn/configs/constants/api_constants.dart';
-import 'package:one_one_learn/core/models/requests/schedule/schedule_by_tutor_id_request.dart';
-import 'package:one_one_learn/core/models/responses/schedule/schedule_by_tutor_id_response.dart';
+import 'package:one_one_learn/core/models/requests/schedule_and_booking/schedule_by_tutor_id_request.dart';
+import 'package:one_one_learn/core/models/responses/schedule_and_booking/schedule_by_tutor_id_response.dart';
 import 'package:one_one_learn/core/network/repositories/base_repository.dart';
 
 class BookingRepository extends BaseRepository {
