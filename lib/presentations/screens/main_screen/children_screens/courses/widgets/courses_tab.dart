7906993daa92e06c83e4d859708fe99e3c2864f5
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-04-28 16:26:53 +0700

    refactor courses tab a little bit

--- lib/presentations/screens/main_screen/children_screens/courses/widgets/courses_tab.dart
+++ lib/presentations/screens/main_screen/children_screens/courses/widgets/courses_tab.dart
@@ -1,20 +1,11 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:one_one_learn/configs/constants/colors.dart';
-import 'package:one_one_learn/presentations/screens/course_information/course_information_screen.dart';
 import 'package:one_one_learn/presentations/screens/main_screen/children_screens/courses/bloc/courses_cubit.dart';
-import 'package:one_one_learn/presentations/screens/main_screen/children_screens/courses/widgets/course_card.dart';
+import 'package:one_one_learn/presentations/screens/main_screen/children_screens/courses/widgets/list_courses.dart';
 import 'package:one_one_learn/presentations/screens/main_screen/widgets/tutors_courses_search_field.dart';
 import 'package:one_one_learn/presentations/widgets/dialogs/bottom_sheet_dialogs/bodies/tutors_courses_list_filter.dart';
-import 'package:one_one_learn/presentations/widgets/others/simple_network_image.dart';
-import 'package:one_one_learn/presentations/widgets/shimmers/fade_shimmer.dart';
-import 'package:one_one_learn/presentations/widgets/text_fields/text_field_outline.dart';
-import 'package:one_one_learn/utils/extensions/app_extensions.dart';
 import 'package:one_one_learn/configs/constants/dimens.dart';
-import 'package:one_one_learn/configs/constants/route_names.dart';
 import 'package:one_one_learn/generated/l10n.dart';
-import 'package:one_one_learn/presentations/widgets/choice_chips/base_choice_chip.dart';
 
 class CoursesTab extends StatefulWidget {
   const CoursesTab({super.key});
@@ -27,18 +18,18 @@ class _CoursesTabState extends State<CoursesTab> with AutomaticKeepAliveClientMi
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return BlocBuilder<CoursesCubit, CoursesState>(
-      builder: (context, state) {
-        return Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: Dimens.getScreenWidth(context) * 0.03,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // search field
-              TutorsCoursesSearchField(
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: Dimens.getScreenWidth(context) * 0.03,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // search field
+          BlocBuilder<CoursesCubit, CoursesState>(
+            builder: (context, state) {
+              return TutorsCoursesSearchField(
                 hintText: S.current.searchHintCourse,
                 isLoadingMore: state.isLoading,
                 listFilterBodyBottomSheet: TutorsCoursesListFilterBottomSheet(
@@ -67,76 +58,17 @@ class _CoursesTabState extends State<CoursesTab> with AutomaticKeepAliveClientMi
                 onSubmitted: (value) {
                   context.read<CoursesCubit>().onSearchTextSubmitted(value);
                 },
-              ),
-
-              SizedBox(height: Dimens.getProportionalHeight(context, 30)),
-
-              // courses list
-              Expanded(
-                child: ListView.builder(
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  itemCount: state.listCourses.length,
-                  itemBuilder: (context, index) {
-                    // tutor card
-                    final item = state.listCourses[index];
-
-                    if (item == null) {
-                      if (index == state.listCourses.length - 3 && !state.isLoading) {
-                        if (kDebugMode) {
-                          print('call api to get more courses at index: $index');
-                        }
-                        context.read<CoursesCubit>().searchListCourses();
-                      }
-                      return const CourseCard(
-                        isLoading: true,
-                        firstChild: ClipRRect(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(15),
-                          ),
-                          child: AppFadeShimmer(radius: 15,),
-                        ),
-                        name: 'name',
-                        description: 'description',
-                        level: 'level',
-                      );
-                    }
-
-                    return CourseCard(
-                      onTap: () {
-                        Navigator.of(context).pushNamed(
-                          RouteNames.courseInformation,
-                          arguments: CourseInformationArguments(
-                            courseId: item.id ?? '',
-                            categories: item.categories?.map((e) => e.title ?? '').toList() ?? [],
-                          ),
-                        );
-                      },
-                      padding: EdgeInsets.zero,
-                      firstChild: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          topRight: Radius.circular(15),
-                        ),
-                        child: SimpleNetworkImage(
-                          url: item.imageUrl ?? '',
-                          errorIconSize: Dimens.getProportionalWidth(context, 50),
-                        ),
-                      ),
-                      name: item.name ?? '',
-                      description: item.description ?? '',
-                      categories: item.categories?.map((e) => e.title ?? '').toList() ?? [],
-                      level: item.level ?? '',
-                      lessons: item.topics?.length ?? 0,
-                    );
-                  },
-                ),
-              ),
-            ],
+              );
+            },
           ),
-        );
-      },
+
+
+          SizedBox(height: Dimens.getProportionalHeight(context, 30)),
+
+          // courses list
+          const Expanded(child: ListCourses()),
+        ],
+      ),
     );
   }
 
---
+++ lib/presentations/screens/main_screen/children_screens/courses/widgets/list_courses.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:one_one_learn/configs/constants/dimens.dart';
+import 'package:one_one_learn/configs/constants/route_names.dart';
+import 'package:one_one_learn/presentations/screens/course_information/course_information_screen.dart';
+import 'package:one_one_learn/presentations/screens/main_screen/children_screens/courses/bloc/courses_cubit.dart';
+import 'package:one_one_learn/presentations/screens/main_screen/children_screens/courses/widgets/course_card.dart';
+import 'package:one_one_learn/presentations/widgets/others/simple_network_image.dart';
+import 'package:one_one_learn/presentations/widgets/shimmers/fade_shimmer.dart';
+
+class ListCourses extends StatelessWidget {
+  const ListCourses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CoursesCubit, CoursesState>(
+      builder: (context, state) {
+        final listCourses = state.listCourses;
+        if (listCourses.isEmpty) {
+          return const Center(
+            child: Text('No courses'),
+          );
+        }
+
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          itemCount: listCourses.length,
+          itemBuilder: (context, index) {
+            // tutor card
+            final item = listCourses[index];
+
+            if (item == null) {
+              if (index == listCourses.length - 3 && !state.isLoading) {
+                if (kDebugMode) {
+                  print('call api to get more courses at index: $index');
+                }
+                context.read<CoursesCubit>().searchListCourses();
+              }
+              return const CourseCard(
+                isLoading: true,
+                firstChild: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: AppFadeShimmer(radius: 15,),
+                ),
+                name: 'name',
+                description: 'description',
+                level: 'level',
+              );
+            }
+
+            return CourseCard(
+              onTap: () {
+                Navigator.of(context).pushNamed(
+                  RouteNames.courseInformation,
+                  arguments: CourseInformationArguments(
+                    courseId: item.id ?? '',
+                    categories: item.categories?.map((e) => e.title ?? '').toList() ?? [],
+                  ),
+                );
+              },
+              padding: EdgeInsets.zero,
+              firstChild: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: SimpleNetworkImage(
+                  url: item.imageUrl ?? '',
+                  errorIconSize: Dimens.getProportionalWidth(context, 50),
+                ),
+              ),
+              name: item.name ?? '',
+              description: item.description ?? '',
+              categories: item.categories?.map((e) => e.title ?? '').toList() ?? [],
+              level: item.level ?? '',
+              lessons: item.topics?.length ?? 0,
+            );
+          },
+        );
+      },
+    );
+  }
+}
