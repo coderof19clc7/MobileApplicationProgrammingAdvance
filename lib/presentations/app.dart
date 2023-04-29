a3715ab65424936f9d5b65ecb3280d8313a7cc0e
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-04-30 00:54:49 +0700

    Update app.dart

--- lib/presentations/app.dart
+++ lib/presentations/app.dart
@@ -5,8 +5,11 @@ import 'package:one_one_learn/configs/app_configs/app_configs.dart';
 import 'package:one_one_learn/configs/app_configs/app_global_navigator.dart';
 import 'package:one_one_learn/configs/app_configs/app_router.dart';
 import 'package:one_one_learn/configs/app_configs/injector.dart';
+import 'package:one_one_learn/configs/constants/route_names.dart';
 import 'package:one_one_learn/configs/stylings/app_themes.dart';
 import 'package:one_one_learn/generated/l10n.dart';
+import 'package:one_one_learn/presentations/screens/main_screen/main_screen.dart';
+import 'package:one_one_learn/presentations/screens/on_boarding/on_boarding_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,8 +30,10 @@ class MyApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      initialRoute: appConfig.initialRoute,
       builder: FToastBuilder(),
+      home: appConfig.initialRoute == RouteNames.onBoarding
+          ? const OnBoardingScreen()
+          : const MainScreen(),
     );
   }
 }
